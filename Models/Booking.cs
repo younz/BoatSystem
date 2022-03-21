fb32456ba70af5790563d327dd415b014e668634
younas kronborg abachri <younas1996@live.dk>
2022-03-21 16:04:30 +0100

    statte op forbindelse til json fil

---
+++ Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_World_Razor_Page.Models
+{
+    public class Booking
+    {
+        public DateTime Date { get; set; } = DateTime.Today;
+
+
+    }
+}
